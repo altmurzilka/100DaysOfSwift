3242bde06985b401b8304127c3074bb67b345d91
altyn <altmurzilka@gmail.com>
2020-08-28 21:38:06 +0600

    day 17 project 1 done!

--- Project1/Project1/DetailViewController.swift
+++ Project1/Project1/DetailViewController.swift
@@ -8,13 +8,36 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+    
+    // challenge 3
+    var selectedPictureNumber = 0
+    var totalPictures = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // challenge 3
+        title = "This image is \(selectedPictureNumber) of \(totalPictures)"
+        navigationItem.largeTitleDisplayMode = .never
 
         // Do any additional setup after loading the view.
+        
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
 
     /*
     // MARK: - Navigation
--- Project1/Project1/ViewController.swift
+++ Project1/Project1/ViewController.swift
@@ -9,8 +9,13 @@ import UIKit
 
 class ViewController: UITableViewController {
     var pictures = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
         let items = try! fm.contentsOfDirectory(atPath: path)
@@ -21,6 +26,9 @@ class ViewController: UITableViewController {
             }
         }
         print(pictures)
+        
+        // challenge 2
+        pictures.sort()
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -33,5 +41,16 @@ class ViewController: UITableViewController {
         return cell
     }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            
+            // challenge 3
+            vc.selectedPictureNumber = indexPath.row + 1
+            vc.totalPictures = pictures.count
+            
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
 }
 
