af10b1c09b162f4ae2a4ed159ea89c00e2ec9e0e
altyn <altmurzilka@gmail.com>
2020-09-02 02:14:18 +0600

    day 23 continue

--- Challenge1/Challenge1/ViewController.swift
+++ Challenge1/Challenge1/ViewController.swift
@@ -7,13 +7,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    var pictures = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        title = "Country flags"
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasSuffix("png") {
+                pictures.append(item)
+            }
+        }
+        print(pictures)
+        
+        pictures.sort()
     }
-
-
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        //cell.textLabel?.text = pictures[indexPath.row]
+        cell.imageView?.image = UIImage(named: pictures[indexPath.row])
+        return cell
+    }
+    
 }
 
