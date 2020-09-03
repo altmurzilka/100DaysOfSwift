5093fe5dbbb7562ee5494e3986f9881844ad8dd0
altyn <altmurzilka@gmail.com>
2020-09-03 22:39:58 +0600

    day 24 start

---
+++ Project4/Project4/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Project4
+//
+//  Created by Алтын on 9/3/20.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
+
