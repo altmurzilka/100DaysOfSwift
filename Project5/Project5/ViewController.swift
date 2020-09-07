6a2a4a31e654a752b5c6ff349d5ea225625726ae
altyn <altmurzilka@gmail.com>
2020-09-07 23:10:11 +0600

    day 27 begin skipping day 26

---
+++ Project5/Project5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project5
+//
+//  Created by Алтын on 9/7/20.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
